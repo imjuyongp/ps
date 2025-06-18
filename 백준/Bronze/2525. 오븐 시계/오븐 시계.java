3a5a8af483ebc74eacecb9208dc47af4cbdf4d7d
JUYONG PARK <158154226+pjuyong@users.noreply.github.com>
2025-06-19 01:07:37 +0900

    [Bronze III] Title: 오븐 시계, Time: 192 ms, Memory: 18400 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,30 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int h = sc.nextInt();
-    int m = sc.nextInt();
-    int t = sc.nextInt();
+    int h = sc.nextInt(); // 현재 시각 (시)
+    int m = sc.nextInt(); // 현재 시각 (분)
+    int t = sc.nextInt(); // 요리 시간 (분)
 
-    if(0<=h && h<=23 && 0<=m && m<=59){
-      h = h + (t/60);
-      if(h>23) {
-        h = h - 24;
-      }
-      m = m + (t%60);
-      if(m >= 60) {
-        h = h + (m/60);
-        if(h>23) {
-          h = h - 24;
-        }
-        m = m % 60;
-      }else if(m == 60) {
-        h = h + 1;
-        m = 0;
-      }
-    }
+    m += t;            // 전체 분 계산
+    h += m / 60;       // 시에 올려줌
+    m = m % 60;        // 60분 넘어간 부분 잘라줌
+    h = h % 24;        // 24시 넘어간 부분 조정
 
     System.out.println(h + " " + m);
   }
