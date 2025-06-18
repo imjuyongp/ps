7405ff12269b91a96ac20953b41ff004e40abcfa
JUYONG PARK <158154226+pjuyong@users.noreply.github.com>
2025-06-19 03:05:11 +0900

    [Bronze IV] Title: 영수증, Time: 184 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int total = sc.nextInt();
+    int num = sc.nextInt();
+    int sum = 0;
+    for(int i = 0; i < num; i++){
+      int price = sc.nextInt();
+      int quantity = sc.nextInt();
+      sum = sum + (price * quantity);
+    }
+    if(sum == total){
+      System.out.println("Yes");
+    } else {
+      System.out.println("No");
+    }
+  }
+}
