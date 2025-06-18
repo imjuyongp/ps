7080cf833d77a866bf7b4780b494868567fac362
JUYONG PARK <158154226+pjuyong@users.noreply.github.com>
2025-06-19 02:56:07 +0900

    [Bronze V] Title: A+B - 3, Time: 188 ms, Memory: 17912 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -3,21 +3,13 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
-    int[] array = new int[a];
 
-    for (int i = 0; i < a; i++) {
-      int n = sc.nextInt();
-      int m = sc.nextInt();
-      int k = n + m;
-      array[i] = k;
+    int T = sc.nextInt();  // 테스트 케이스 수
+
+    for (int i = 0; i < T; i++) {
+      int A = sc.nextInt();
+      int B = sc.nextInt();
+      System.out.println(A + B);
     }
-
-    for(int i = 0; i < array.length; i++){
-      System.out.println(array[i]);
-    }
-
-
-
   }
 }
