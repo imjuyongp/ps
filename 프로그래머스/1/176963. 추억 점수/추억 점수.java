5f4aacaaf69f51f66b9b5e3cf9adab5313a19f9e
JUYONG <imjuyongp@gmail.com>
2026-04-24 10:46:20 +0900

    [level 1] Title: 추억 점수, Time: 1.56 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,21 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        
         int[] result = new int[photo.length];
+        HashMap<String,Integer> map = new HashMap<>();
         
-        for (int i = 0; i < photo.length; i++) {
-            int sum = 0; 
-            
-            for (int j = 0; j < photo[i].length; j++) {
-                
-                for (int k = 0; k < name.length; k++) {
-                    if (photo[i][j].equals(name[k])) {
-                        sum += yearning[k];
-                    }
+        for (int i=0; i<name.length; i++) {
+            map.put(name[i], yearning[i]); // 이름 : 추억 점수 HashMap으로 저장
+        }
+        
+        for (int i=0; i<photo.length; i++) {
+            int sum=0;
+            for(int j=0; j<photo[i].length; j++) {
+                if(map.containsKey(photo[i][j])) {
+                    sum += map.get(photo[i][j]);
                 }
             }
-            
-            result[i] = sum;
+            result[i] += sum;
         }
         
         return result;
