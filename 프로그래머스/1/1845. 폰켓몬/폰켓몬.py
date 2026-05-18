a620fec56eb10772ef3fc8e527c2d0514889d52b
JUYONG <imjuyongp@gmail.com>
2026-05-18 10:30:50 +0900

    [level 1] Title: 폰켓몬, Time: 0.54 ms, Memory: 9.78 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,16 +1,19 @@
 def solution(nums):
     answer = 0
-    check = [] # 폰켄몬 종류 저장 리스트
-    dic = {pok : 0 for pok in nums} # 폰켓몬 마리수를 0으로 초기화 
-    for pok in nums: # 폰켓몬 마리 수를 저장
-        dic[pok] += 1
-    for i, pok in enumerate(dic):
-        if(i>=len(nums)//2):
-            break
-        if(dic[pok]==0):
-            continue
-        answer += 1 
-        dic[pok] -= 1
-        
-        
-    return answer
+    #dic = {pok : 0 for pok in nums} # 폰켓몬 마리수를 0으로 초기화 
+    #for pok in nums: # 폰켓몬 마리 수를 저장
+    #    dic[pok] += 1
+    #for i, pok in enumerate(dic):
+    #    if(i>=len(nums)//2):
+    #        break
+    #    if(dic[pok]==0):
+    #        continue
+    #    answer += 1 
+    #    dic[pok] -= 1
+    
+    pok = {} # 종류 저장 딕셔너리
+    for i in nums:
+        if(i not in pok):
+            pok[i] = True
+    return min(len(nums)//2, len(pok))     
+    #return answer
