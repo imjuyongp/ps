09513bc25ed3e86620c1ccfc0d1583ce8002da83
JUYONG <imjuyongp@gmail.com>
2026-06-24 11:03:18 +0900

    [level 1] Title: 모의고사, Time: 1.43 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840#) 
 
 ### 성능 요약
 
-메모리: 11.7 MB, 시간: 1.04 ms
+메모리: 11.7 MB, 시간: 1.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 24일 10:55:22
+2026년 06월 24일 11:03:16
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -4,22 +4,16 @@ def solution(answers):
     p1 = [1,2,3,4,5] # 5
     p2 = [2,1,2,3,2,4,2,5] # 8
     p3 = [3,3,1,1,2,2,4,4,5,5] # 10
-    
-    c1,c2,c3 = 0,0,0
+    score = [0,0,0]
     
     for i in range(pn):
         if(answers[i] == p1[i%5]):
-            c1 += 1
+            score[0] += 1
         if(answers[i] == p2[i%8]):
-            c2 += 1
+            score[1] += 1
         if(answers[i] == p3[i%10]):
-            c3 += 1
-        
-    score = [c1,c2,c3]
+            score[2] += 1
+            
     max_score = max(score)
     
-    for idx,c in enumerate(score):
-        if(c == max_score):
-            result.append(idx+1)
-    
-    return result
+    return [idx+1 for idx, c in enumerate(score) if (c == max_score)]
