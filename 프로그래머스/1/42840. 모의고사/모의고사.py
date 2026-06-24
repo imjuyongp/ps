0f0eadeac3b6b83bb6ab89ce1274b7aac5c7ebed
JUYONG <imjuyongp@gmail.com>
2026-06-24 10:23:32 +0900

    [level 1] Title: 모의고사, Time: 1.00 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,24 @@
+def solution(answers):
+    result = []
+    pn = len(answers) # 문제수
+    p1 = [1,2,3,4,5] # 5
+    p2 = [2,1,2,3,2,4,2,5] # 8
+    p3 = [3,3,1,1,2,2,4,4,5,5] # 10
+    
+    c1,c2,c3 = 0,0,0
+    
+    for i in range(pn):
+        if(answers[i] == p1[i%5]):
+            c1 += 1
+        if(answers[i] == p2[i%8]):
+            c2 += 1
+        if(answers[i] == p3[i%10]):
+            c3 += 1
+        
+    score = [c1,c2,c3]
+    max_score = max(score)
+    for i in range(3):
+        if(score[i] == max_score):
+            result.append(i+1)
+    
+    return result
