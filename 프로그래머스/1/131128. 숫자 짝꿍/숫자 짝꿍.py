eea79cbe4c48903cf36942440a69b7bff700d594
JUYONG <imjuyongp@gmail.com>
2026-06-26 13:37:32 +0900

    [level 1] Title: 숫자 짝꿍, Time: 150.73 ms, Memory: 45.2 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,11 +1,7 @@
-from collections import Counter
-
 def solution(X, Y):
-    cx = Counter(X)
-    cy = Counter(Y)
     result = []
     for i in range(9,-1,-1):
-        result.extend(str(i) * min(cx[str(i)], cy[str(i)]))
+        result.extend(str(i) * min(X.count(str(i)), Y.count(str(i))))
     if not result:
         return '-1'
     result.sort(reverse=True)
